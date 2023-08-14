c9e3e9b6ffe4e10984639316ef7e1cb19501f320
pravinkolambkar <94437199+pravinkolambkar@users.noreply.github.com>
2023-08-14 13:39:40 +0530

    commit on 14.08.23

---
+++ src/app/home/popular-jobs/popular-jobs.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-popular-jobs',
+  templateUrl: './popular-jobs.component.html',
+  styleUrls: ['./popular-jobs.component.css']
+})
+export class PopularJobsComponent {
+
+}
