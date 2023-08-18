73a3f9519afb07cd52a2af10dc2cd7588016fa8f
chittaranjan-dev <ranjan74feb@yahoo.co.in>
2023-08-18 19:31:50 +0530

    Added our trades section and footer

---
+++ src/app/home/our-services/our-services.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { TradepersonJob } from '../models/tradepersonjob.model';
+import  tradepersonjobData from '../../../assets/JsonFiles/TradePersonJobs.json';
+
+@Component({
+  selector: 'app-our-services',
+  templateUrl: './our-services.component.html',
+  styleUrls: ['./our-services.component.css']
+})
+export class OurServicesComponent implements OnInit {  
+  tradepersons: TradepersonJob[] = [];
+  filteredTradeperson: TradepersonJob[] | undefined = undefined;
+  selectedTradepersonId: number | any;
+
+  constructor(private router: Router) {} 
+
+    ngOnInit() {
+        this.getfilteredTradepersons();
+    }
+
+  getfilteredTradepersons() {
+    this.tradepersons = tradepersonjobData;
+    this.filteredTradeperson = this.tradepersons.filter(
+      (trperson) => trperson.ParentId === null
+    );
+  }
+
+  onClick() {
+    // this.router.navigate(['/post-a-job', 2, 4, 1]);
+  }
+}
