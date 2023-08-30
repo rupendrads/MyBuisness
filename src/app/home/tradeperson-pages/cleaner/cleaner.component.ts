9377af27dba97fc2a12d70b210dba6a99b891d64
Rupendra Sawant <Rupendra.Sawant@fefundinfo.com>
2023-08-30 12:04:04 +0100

    Added Meta tag

--- src/app/home/tradeperson-pages/cleaner/cleaner.component.ts
+++ src/app/home/tradeperson-pages/cleaner/cleaner.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-cleaner',
@@ -7,7 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CleanerComponent implements OnInit{
   
-  constructor() {};
+  constructor(private meta: Meta, private titleService: Title) {
+    this.titleService.setTitle('Cleaner : Top cleaner near me');
+    this.meta.addTag({
+      name: 'Cleaner',
+      content: 'Top Builders Near Me. Quickly connect with the best builders companies in your area.'
+    })
+    
+    this.meta.addTag({
+      property: 'og:url',
+      content:'https://topbuildernearme.co.uk/cleaner'
+   })
+      
+   this.meta.addTag({
+      property: 'og:description',
+      content:'Need a Cleaner? Find the best Cleaner in minutes. We have found the top Cleaner near you. Search in seconds, read reviews &amp; get free quotes. Post your request today!'
+   })
+
+   this.meta.addTag({
+    property: 'twitter:description',
+    content:'Need a Cleaner? Find the best Cleaner in minutes. We have found the top Cleaner near you. Search in seconds, read reviews &amp; get free quotes. Post your request today!'
+   })
+    
+  } 
 
   ngOnInit() {    
   };
--- src/app/home/tradeperson-pages/electrician/electrician.component.ts
+++ src/app/home/tradeperson-pages/electrician/electrician.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-electrician',
@@ -8,6 +9,28 @@ import { Component, OnInit } from '@angular/core';
 export class ElectricianComponent implements OnInit{
   ngOnInit() {};
 
-  constructor() {};
+  constructor(private meta: Meta, private titleService: Title) {
+    this.titleService.setTitle('Electrician : Top electrician near me');
+    this.meta.addTag({
+      name: 'Electrician',
+      content: 'Top Builders Near Me. Quickly connect with the best builders companies in your area.'
+    })
+    
+    this.meta.addTag({
+      property: 'og:url',
+      content:'https://topbuildernearme.co.uk/electrician'
+   })
+      
+   this.meta.addTag({
+      property: 'og:description',
+      content:'Need a Electrician? Find the best Electrician in minutes. We have found the top Electrician near you. Search in seconds, read reviews &amp; get free quotes. Post your request today!'
+   })
+
+   this.meta.addTag({
+    property: 'twitter:description',
+    content:'Need a Electrician? Find the best Electrician in minutes. We have found the top Electrician near you. Search in seconds, read reviews &amp; get free quotes. Post your request today!'
+   })
+    
+  } 
 
 }
